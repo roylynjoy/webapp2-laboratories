0632811453c491a766eeee1d2dd8217d4eae0ce9
roylynjoy <166943952+roylynjoy@users.noreply.github.com>
2024-05-15 16:51:22 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php/index.php
@@ -0,0 +1,48 @@
+<?php
+//Task1
+$name = 'Roylyn Joy E. Dicdican';
+$age = 20;
+$address = 'Sitio Greenforest brgy. Dela Paz Antipolo City, Rizal';
+$hobbies = ('Drawing, Singing, Dancing, Volleyball, Watching Movies and Kdramas');
+$pet_peeve = 'people who invalidate someone else feeling';
+
+echo $name, '<br>';
+echo $age, '<br>';
+echo $address, '<br>';
+echo $hobbies, '<br>';
+echo $pet_peeve, '<br><br>';
+
+//Task2
+$name = 'Roylyn Joy E. Dicdican';
+$age = 20;
+$address = 'Sitio Greenforest brgy. Dela Paz Antipolo City, Rizal';
+//$hobbies = ('Drawing, Singing, Dancing, Volleyball, Watching Movies and Kdramas');
+//$pet_peeve = 'people who invalidate someone else feeling';
+
+
+//Task3
+define('NAME', 'Roylyn Joy E. Dicdican');
+define('AGE', 20);
+define('ADDRESS', 'Sitio Greenforest brgy. Dela Paz Antipolo City, Rizal');
+define('HOBBIES', 'Drawing, Singing, Dancing, Volleyball, Watching Movies and Kdramas');
+define('PET_PEEVE', 'people who invalidate someone else feeling');
+
+echo NAME, '<br>';
+echo AGE, '<br>';
+echo ADDRESS, '<br>';
+echo HOBBIES, '<br>';
+echo PET_PEEVE, '<br>';
+
+
+//Task4
+echo '<pre>';
+var_dump (NAME, AGE, ADDRESS, HOBBIES, PET_PEEVE);
+echo '</pre>';
+
+
+//Task5
+/* 
+because I feel sad when someone I know feels invalidated 
+(because I used to better understand someone's behavior in accordance with what they feel), 
+and I also know how it felts.
+*/
