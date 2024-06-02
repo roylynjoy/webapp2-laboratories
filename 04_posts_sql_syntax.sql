7534663267250c8e2bba094d7840b798aa762ffa
roylynjoy <166943952+roylynjoy@users.noreply.github.com>
2024-06-02 10:46:06 +0800

    Update 04_posts_sql_syntax.sql

--- 04_posts_sql_syntax.sql
+++ 04_posts_sql_syntax.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE `webapp2-db`;
 
 CREATE TABLE posts(
-    user_Id INT,
+    user_id INT,
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255),
     body VARCHAR(255),  
-    FOREIGN KEY (user_Id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-INSERT INTO posts(user_Id, title, body) VALUES (1, "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"),
+INSERT INTO posts(user_id, title, body) VALUES (1, "sunt aut facere repellat provident occaecati excepturi optio reprehenderit", "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"),
 (1, "qui est esse", "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"),
 (1, "ea molestias quasi exercitationem repellat qui ipsa sit aut", "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"),
 (1, "eum et est occaecati", "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"),
